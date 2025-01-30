a359c4db427e1ed90af8b000c548776852344ec9
Szomti <piotrpiotr1285@gmail.com>
2025-01-30 12:43:14 +0100

    Update Version

--- lib/global.dart
+++ lib/global.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Global {
-  static const String version = '1.0.0';
+  static const String version = '1.0.1';
 
   static Future<void> restartApp(BuildContext context) =>
       Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
